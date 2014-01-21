582f85a7f89a6a5400ae4fd975daa21ba00e7555
Yin Wang <yinwang0@gmail.com>
2014-01-20 22:38:43 -0800

    completely separate define and assign

--- src/main/java/org/yinwang/yin/Binder.java
+++ src/main/java/org/yinwang/yin/Binder.java
@@ -9,50 +9,37 @@ import org.yinwang.yin.value.Value;
 
 public class Binder {
 
-    public static void bind(Node pattern, Value value, Scope env, boolean assign) {
+    public static void define(Node pattern, Value value, Scope env) {
         if (pattern instanceof Name) {
-            if (assign) {
-                assignName((Name) pattern, value, env);
+            String id = ((Name) pattern).id;
+            Value v = env.lookupLocal(id);
+            if (v != null) {
+                _.abort(pattern, "trying to redefine name: " + id);
             } else {
-                defineName((Name) pattern, value, env);
+                env.put(id, value);
+            }
+        } else {
+            _.abort(pattern, "unsupported pattern of define: " + pattern);
+        }
+    }
+
+
+    public static void assign(Node pattern, Value value, Scope env) {
+        if (pattern instanceof Name) {
+            String id = ((Name) pattern).id;
+            Scope d = env.findDefiningScope(id);
+
+            if (d == null) {
+                _.abort(pattern, "assigned name was not defined: " + id);
+            } else {
+                d.put(id, value);
             }
         } else if (pattern instanceof Subscript) {
             ((Subscript) pattern).set(value, env);
         } else if (pattern instanceof Attr) {
             ((Attr) pattern).set(value, env);
-        }
-    }
-
-
-    public static void define(Node pattern, Value value, Scope env) {
-        bind(pattern, value, env, false);
-    }
-
-
-    public static void assign(Node pattern, Value value, Scope env) {
-        bind(pattern, value, env, true);
-    }
-
-
-    public static void defineName(Name name, Value value, Scope env) {
-        String id = name.id;
-        Value v = env.lookupLocal(id);
-        if (v != null) {
-            _.abort(name, "trying to redefine name: " + id);
         } else {
-            env.put(id, value);
-        }
-    }
-
-
-    public static void assignName(Name name, Value value, Scope env) {
-        String id = name.id;
-        Scope d = env.findDefiningScope(id);
-
-        if (d == null) {
-            _.abort(name, "assigned name was not defined: " + id);
-        } else {
-            d.put(id, value);
+            _.abort(pattern, "unsupported pattern of assign: " + pattern);
         }
     }
 
