b65467decf7a51fa38e4b8078cced6bb9fac6916
Yin Wang <yinwang0@gmail.com>
2014-01-21 13:21:20 -0800

    create new scope in blocks

--- src/main/java/org/yinwang/yin/ast/Block.java
+++ src/main/java/org/yinwang/yin/ast/Block.java
@@ -18,6 +18,7 @@ public class Block extends Node {
 
 
     public Value interp(Scope s) {
+        s = new Scope(s);
         for (int i = 0; i < statements.size() - 1; i++) {
             statements.get(i).interp(s);
         }
