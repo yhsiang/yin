4243a84a11ffe5ab339b243c1cd9df03dfd24893
Yin Wang <yinwang0@gmail.com>
2014-01-20 22:45:49 -0800

    vector define and assign

--- src/main/java/org/yinwang/yin/Binder.java
+++ src/main/java/org/yinwang/yin/Binder.java
@@ -1,11 +1,11 @@
 package org.yinwang.yin;
 
 
-import org.yinwang.yin.ast.Attr;
-import org.yinwang.yin.ast.Name;
-import org.yinwang.yin.ast.Node;
-import org.yinwang.yin.ast.Subscript;
+import org.yinwang.yin.ast.*;
 import org.yinwang.yin.value.Value;
+import org.yinwang.yin.value.Vector;
+
+import java.util.List;
 
 public class Binder {
 
@@ -17,6 +17,19 @@ public class Binder {
                 _.abort(pattern, "trying to redefine name: " + id);
             } else {
                 env.put(id, value);
+            }
+        } else if (pattern instanceof VectorLiteral) {
+            if (value instanceof Vector) {
+                List<Node> elms1 = ((VectorLiteral) pattern).elements;
+                List<Value> elms2 = ((Vector) value).values;
+                if (elms1.size() == elms2.size()) {
+                    for (int i = 0; i < elms1.size(); i++) {
+                        define(elms1.get(i), elms2.get(i), env);
+                    }
+                } else {
+                    _.abort(pattern,
+                            "define with vectors of different sizes: " + elms1.size() + " v.s. " + elms2.size());
+                }
             }
         } else {
             _.abort(pattern, "unsupported pattern of define: " + pattern);
@@ -38,6 +51,18 @@ public class Binder {
             ((Subscript) pattern).set(value, env);
         } else if (pattern instanceof Attr) {
             ((Attr) pattern).set(value, env);
+        } else if (pattern instanceof VectorLiteral) {
+            if (value instanceof Vector) {
+                List<Node> elms1 = ((VectorLiteral) pattern).elements;
+                List<Value> elms2 = ((Vector) value).values;
+                if (elms1.size() == elms2.size()) {
+                    for (int i = 0; i < elms1.size(); i++) {
+                        assign(elms1.get(i), elms2.get(i), env);
+                    }
+                } else {
+                    _.abort(pattern, "assign vectors of different sizes: " + elms1.size() + " v.s. " + elms2.size());
+                }
+            }
         } else {
             _.abort(pattern, "unsupported pattern of assign: " + pattern);
         }
