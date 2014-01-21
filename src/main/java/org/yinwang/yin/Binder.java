d6d3624e2e686dd86af9564788c422917f1191bd
Yin Wang <yinwang0@gmail.com>
2014-01-20 18:20:36 -0800

    disallow assignment to undefined names

--- src/main/java/org/yinwang/yin/Binder.java
+++ src/main/java/org/yinwang/yin/Binder.java
@@ -7,26 +7,29 @@ import org.yinwang.yin.value.Value;
 
 public class Binder {
 
-    public static void bind(Node pattern, Value value, Scope env, boolean norewrite) {
+    public static void bind(Node pattern, Value value, Scope env, boolean assign) {
         if (pattern instanceof Name) {
             String id = ((Name) pattern).id;
             Value v = env.lookupLocal(id);
-            if (v == null || !norewrite) {
-                env.put(id, value);
-            } else {
+
+            if (assign && v == null) {
+                _.abort(pattern, "assigned name was not defined: " + id);
+            } else if (!assign && v != null) {
                 _.abort(pattern, "trying to redefine name: " + id);
+            } else {
+                env.put(id, value);
             }
         }
     }
 
 
     public static void assign(Node pattern, Value value, Scope env) {
-        bind(pattern, value, env, false);
+        bind(pattern, value, env, true);
     }
 
 
     public static void def(Node pattern, Value value, Scope env) {
-        bind(pattern, value, env, true);
+        bind(pattern, value, env, false);
     }
 
 }
