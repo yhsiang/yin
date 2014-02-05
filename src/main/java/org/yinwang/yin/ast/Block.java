950bffe1fd28c74406fafcd8da3ee8815d74746c
Yin Wang <yinwang0@gmail.com>
2014-02-05 00:26:09 -0800

    small fix

--- src/main/java/org/yinwang/yin/ast/Block.java
+++ src/main/java/org/yinwang/yin/ast/Block.java
@@ -30,7 +30,7 @@ public class Block extends Node {
     public Value typecheck(Scope s) {
         s = new Scope(s);
         for (int i = 0; i < statements.size() - 1; i++) {
-            statements.get(i).interp(s);
+            statements.get(i).typecheck(s);
         }
         return statements.get(statements.size() - 1).typecheck(s);
     }
